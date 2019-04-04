09560e5a38cf91b440e1354b4ad642e1f5b25e53
anirbmuk <anirbmuk@live.in>
2019-04-04 13:55:59 +0530

    Added custom chart details into context menu action

--- src/js/common/composites/hr-chart/hr-chart.js
+++ src/js/common/composites/hr-chart/hr-chart.js
@@ -1,6 +1,11 @@
 define(['ojs/ojcore',
         'knockout',
-        'ojs/ojchart'],
+        'ojs/ojchart',
+        'ojs/ojdialog',
+        'ojs/ojbutton',
+        'ojs/ojlabel',
+        'ojs/ojinputtext',
+        'ojs/ojdatetimepicker'],
 function(oj, ko) {
     
     function HrChartModel(context) {
@@ -9,8 +14,11 @@ function(oj, ko) {
         self.composite = context.element;
         self.properties = context.properties;
         
+        self.showChartContextMenu = ko.observable(self.properties.contextMenu.showContextMenu);
+        self.contextMenu = ko.observable(self.properties.contextMenu);
+        
         self.chartComponentId = ko.observable(self.properties.chartComponentId);
-        self.chartSeriesValue = ko.observableArray(self.properties.chartSeriesValue);
+        self.chartSeriesValue = self.properties.chartSeriesValue;
         self.chartGroupsValue = ko.observableArray(self.properties.chartGroupsValue);
         
         self.styleDefaults = self.properties.styleDefaults;
@@ -26,8 +34,41 @@ function(oj, ko) {
         self.drilling = self.chartDefaults.drilling;
         self.sorting = self.chartDefaults.sorting;
         
+        self.chartItem = ko.observable({});
+        
+        self.filterGroupTemplate = self.properties.filterOptions;
+        
+        self.onFilterDialogOK = function() {
+            self.filterGroupTemplate.onFilterDialogOK();
+            $('#filterModal')[0].close();
+        };
+        
+        self.onFilterDialogCancel = function() {
+            $('#filterModal')[0].close();
+        };
+        
         self.onOjDrill = function(event) {
             self.handleChartSelection(event.detail);
+            event.stopPropagation();
+        };
+        
+        self.beforeContextMenu = function(event) {
+            const target = event.detail.originalEvent.target;
+            const chart = document.getElementById(self.chartComponentId());
+            if (chart) {
+                const context = chart.getContextByNode(target);
+                if (context && context.subId === "oj-chart-item") {
+                    self.chartItem(chart.getDataItem(context["seriesIndex"], context["itemIndex"]));
+                }
+            }
+        };
+        
+        self.contextMenuAction = function(event) {
+             if (event.target && event.target.value === 'action1') {
+                $('#filterModal')[0].open();
+            } else {
+                self.handleContextMenuAction(event.target);
+            }
             event.stopPropagation();
         };
         
@@ -46,6 +87,24 @@ function(oj, ko) {
         };
         
         self.composite.dispatchEvent(new CustomEvent('drillDown', eventParams));        
+    };
+    
+    HrChartModel.prototype.handleContextMenuAction = function(detail) {
+        const self = this;
+        
+        const customDetail = JSON.parse(JSON.stringify(detail));
+        customDetail.itemSelected = detail;
+        customDetail.selectedValue = detail.value;
+        customDetail.series = self.chartItem().series;
+        customDetail.group = self.chartItem().group;
+        customDetail.value = self.chartItem().value;
+        const eventParams = {
+            bubbles: true,
+            cancelable: false,
+            detail: customDetail
+        };
+        
+        self.composite.dispatchEvent(new CustomEvent('contextMenuAction', eventParams));        
     };
     
     return HrChartModel;
--- src/js/routes/model/demo.js
+++ src/js/routes/model/demo.js
@@ -51,13 +51,75 @@ define(['ojs/ojcore',
                 threeDEffect: 'off'
             };
             self.pieLegend = {
-                rendered: 'off'
+                rendered: 'on'
+            };
+            self.contextMenu = {
+                showContextMenu: true,
+                options: [
+                    {
+                        id: "contextMenuOption1",
+                        label: "Filter",
+                        value: "action1"
+                    },
+                    {
+                        id: "contextMenuOption2",
+                        label: "Action 2",
+                        value: "action3"
+                    },
+                    {
+                        id: "contextMenuOption2",
+                        label: "Action 3",
+                        value: "action3"
+                    }
+                ]
+            };
+            self.filterOptions = {
+                onFilterDialogOK: function() {
+                    self.pieSeriesValue([
+                    {
+                        name: "Administration", items: [{ id: 10, value: 10 }]
+                    },
+                    {
+                        name: "Shipping", items: [{ id: 20, value: 25 }]
+                    },
+                    {
+                        name: "IT Support", items: [{ id: 30, value: 0 }]
+                    },
+                    {
+                        name: "Payroll", items: [{ id: 40, value: 0 }]
+                    }
+                ]);
+                },
+                value: {
+                    groups: [
+                        {
+                            component: 'ojInputDate',
+                            id: 'oinput2',
+                            label: 'Start Date',
+                            inputValue: '',
+                            isRequired: false,
+                            visible: true
+                        },
+                        {
+                            component: 'ojInputDate',
+                            visible: true,
+                            id: 'oinput3',
+                            label: 'End Date',
+                            inputValue: '',
+                            isRequired: false
+                        }
+                    ]
+                }
             };
             
             self.init = function() {
             };
             
             self.onDrillDownAction = function(event) {
+                console.log(event);
+            };
+            
+            self.contextMenuAction = function(event) {
                 console.log(event);
             };
 
